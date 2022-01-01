a58142b114824edfa977285831dd2a285e4677fb
Omda <Omda18009@gmail.com>
2022-01-01 13:37:43 -0800

    SecondHome

---
+++ BeautySalons/Controllers/BeautyController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeautySalons.Controllers
+{
+    public class BeautyController : Controller
+    {
+        public IActionResult IndexBeauty()
+        {
+            return View();
+        }
+    }
+}
